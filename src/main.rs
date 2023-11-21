e4bf4f75b1e81329bf0f1ea26bec9b8be2bbc64d
Kelvin Chin <chinkaiwen@yahoo.com>
2023-11-22 00:08:51 +0800

    Exploring methods to store token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let token: String = match env::var("BOT_TOKEN") {
+        Ok(val) => val,
+        Err(e) => panic!("Error: failed to get token, {}", e),
+    };
+    print!("bot token is: {}", token);
 }
