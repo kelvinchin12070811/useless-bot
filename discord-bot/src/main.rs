b98f3d45f762717e3536c0def81c112148b72d09
Kelvin Chin <chinkaiwen@yahoo.com>
2023-12-15 02:15:32 +0800

    Added logging

--- discord-bot/src/main.rs
+++ discord-bot/src/main.rs
@@ -4,28 +4,45 @@ mod types;
 
 use commands::get_commands;
 use config::Config;
+use env_logger::Builder;
+use log::{error, info, LevelFilter};
 use poise::serenity_prelude as serenity;
 use std::env;
 use types::Data;
 
 #[tokio::main]
 async fn main() {
+    Builder::new()
+        .filter_module("useless_bot", LevelFilter::Trace)
+        .init();
+
+    info!("Starting useless-bot...");
+    info!("Reading environment variable...");
     let config = Config {
         bot_token: match env::var("BOT_TOKEN") {
             Ok(val) => val,
-            Err(_e) => panic!("Error, token is required but it seems to be not provided!"),
+            Err(_e) => {
+                error!("Error, token is required but it seems to be not provided!");
+                panic!()
+            }
         },
         api_username: match env::var("API_USERNAME") {
             Ok(val) => val,
-            Err(_e) => panic!("Error, username is required but it seems to be not provided!"),
+            Err(_e) => {
+                error!("Error, username is required but it seems to be not provided!");
+                panic!()
+            }
         },
         api_password: match env::var("API_PASSWORD") {
             Ok(val) => val,
-            Err(_e) => panic!("Error, password is required but it seems to be not provided!"),
+            Err(_e) => {
+                error!("Error, password is required but it seems to be not provided!");
+                panic!()
+            }
         },
     };
 
-    println!("{:?}", config.bot_token);
+    info!("Building framework...");
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
             commands: get_commands::get_commands(),
@@ -40,5 +57,6 @@ async fn main() {
             })
         });
 
+    info!("Running framework...");
     framework.run().await.unwrap();
 }
