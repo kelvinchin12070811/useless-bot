ccd02515478f73367bd09cb9e865b5e5e981067c
Kelvin Chin <chinkaiwen@yahoo.com>
2023-12-15 19:53:41 +0800

    Added stickers command

--- discord-bot/src/commands/get_commands.rs
+++ discord-bot/src/commands/get_commands.rs
@@ -3,8 +3,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  **********************************************************************************************************************/
-use crate::{commands::common, types::Error};
+use crate::{
+    commands::{common, stickers},
+    types::Error,
+};
 
 pub fn get_commands() -> Vec<poise::Command<crate::types::Data, Error>> {
-    vec![common::ping()]
+    vec![common::ping(), stickers::sticker()]
 }
--- discord-bot/src/commands/stickers.rs
+++ discord-bot/src/commands/stickers.rs
@@ -3,5 +3,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  **********************************************************************************************************************/
-pub mod api_service;
-pub mod sticker_service;
+
+use crate::types::{Context, Result};
+
+#[poise::command(slash_command, subcommands("list"))]
+pub async fn sticker(ctx: Context<'_>) -> Result {
+    ctx.say("Stickers are so coo!").await?;
+    Ok(())
+}
+
+/// List all the available stickers.
+#[poise::command(slash_command)]
+pub async fn list(ctx: Context<'_>) -> Result {
+    ctx.say("hello world").await?;
+    Ok(())
+}
