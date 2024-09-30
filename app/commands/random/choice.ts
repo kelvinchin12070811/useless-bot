13876cefe3bc5865e364f35bda9dd1e0bc0f9fda
Kelvin Chin <chinkaiwen_35@outlook.com>
2024-09-30 23:52:11 +0800

    Make fancy result from the choice result

--- app/commands/random/choice.ts
+++ app/commands/random/choice.ts
@@ -3,12 +3,38 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  **********************************************************************************************************************/
+import { logger } from '../../logger';
 import { CommandReducer } from '../command';
+
+/**
+ * Formate the outcome message to display the choices from the list from a list of template randomly.
+ * @param choice The choice that was selected.
+ * @param choices The list of choices to choose from.
+ * @returns The formatted message.
+ */
+function randomMessage(choice: string, choices: string[]): string {
+    const limit = 3;
+    const index = Math.floor(Math.random() * limit);
+
+    switch (index) {
+        case 0:
+            return `I choose \`${choice}\` from the list of choices: \`${choices.join(', ')}\``;
+        case 1:
+            return `I think \`${choice}\` is the best choice from the list of choices: \`${choices.join(', ')}\``;
+        case 2:
+            return `I would go with \`${choice}\` from the list of choices: \`${choices.join(', ')}\``;
+        default:
+            return '';
+    }
+}
 
 export const choice: CommandReducer = async interaction => {
     const choices: string[] = (interaction.options.getString('choices') ?? '')
         .split(',')
-        .map((choice: string) => choice.trim());
+        .map((choice: string) => choice.trim())
+        .filter((choice: string) => choice.length > 0);
+
+    logger.debug(`[random][choice] Length: ${choices.length}, Choices: ${JSON.stringify(choices)}`);
 
     if (choices.length <= 0) {
         await interaction.reply({
@@ -19,5 +45,5 @@ export const choice: CommandReducer = async interaction => {
     }
 
     const choice = choices[Math.floor(Math.random() * choices.length)];
-    await interaction.reply(choice);
+    await interaction.reply(randomMessage(choice, choices));
 };
